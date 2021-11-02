e3a98193118dc57be36c6037ee91aee57c4c0f87
priyankarawat6250 <priyankarawat6250@gmail.com>
2021-11-02 12:58:57 +0530

    React Chat Node

---
+++ react/config.js
@@ -0,0 +1,22 @@
+module.exports={
+  'googleAuth' : {
+  'clientID' : '127430973269-sroinqafd4ab6ujek55g22b5g5875orb.apps.googleusercontent.com',
+  'clientSecret' : '6lRYAnWM1aSHp6VjdQeRKxJu',
+    'callbackURL': 'http://192.168.1.7:3000/',
+    'backURL': 'http://192.168.1.7:8080/api/',
+    'backend': 'http://192.168.1.7:8080/',
+  },
+  'fbauth':{
+  'appId':"2304012543172252"
+  },
+  'password_encrypt':{
+  'hash':'md5'
+  },
+  'login_secret':{
+  'key':'pokemon'
+  },
+  
+  'login_secretadmin':{
+    'key':'pokemonlove'
+    },
+  }
